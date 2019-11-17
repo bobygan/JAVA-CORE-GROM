ef51e6ca8f6a2fed29afebd7fb8a3b6010731e1a
bobygan <a.e.podgorny@gmail.com>
2019-11-17 18:59:13 +0200

    1r

--- lesson30/homework2/CustomerDAO.java
+++
@@ -1,9 +0,0 @@
-package lesson30.homework2;
-
-public class CustomerDAO {
-    public static Customer customerIvan=new Customer("Ivan","UA",100);
-
-    public static Customer customerJon=new Customer("Jon","US",100);
-
-
-}
